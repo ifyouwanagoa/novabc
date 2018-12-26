e36190fd75c0a877cd447e67b6bed0498ccf34af
ifyouwanagoa <42785783+ifyouwanagoa@users.noreply.github.com>
2018-12-26 21:02:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,31 +14,30 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log('I am ready!');
 });
-client.on('message',async message => {
+client.on('message',async message => {//Toxic Code
   if(message.author.bot || message.channel.type === '!bc') return;
   let args = message.content.split(' ');
-  if(args[0] === `!bc`) {
+  if(args[0] === `!bc`) {//Toxic Code
     if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send('- **أنت لا تملك الصلاحيات اللازمة لأستخدام هذا الأمر**');
     if(!args[1]) return message.channel.send('- **يجب عليك كتابة الرسالة بعد الأمر**');
  
     let msgCount = 0;
     let errorCount = 0;
     let successCount = 0;
-    message.channel.send(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`).then(msg => {
-      message.guild.members.forEach(g => {
-        g.send(args.slice(1).join(' ')).then(() => {
+    message.channel.send(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`).then(msg => {//Toxic Code
+      message.guild.members.forEach(g => {//Toxic Code
+        g.send(args.slice(1).join(' ')).then(() => {//Toxic Code
           successCount++;
           msgCount++;
           msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
         }).catch(e => {
           errorCount++;
           msgCount++;
-          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
-        });
-      });
-    });
+          msg.edit(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);//Toxic Code
+        });//Toxic Code
+      });//Toxic Code
+    });//Toxic Code
   }
-});
+});//Toxic Code
 
-	 
 client.login(process.env.BOT_TOKEN);
